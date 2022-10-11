ad0f2d6827143fbd51c5ca61f0053d798dfd9458
Rxyxna <Rayanabaga@outlook.com>
2022-10-11 09:39:00 +0200

    DATA

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ const ctx = document.querySelector('#chart').getContext('2d');
 const chart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['USA', 'CANADA', 'FRANCE', 'BRAZIL', 'GERMANY'],
         datasets: [{
-            data: [12, 19, 3, 5, 2, 3],
+            data: [523.06, 303.96, 195.10, 190.10, 156.48],
         }]
     }
 });
